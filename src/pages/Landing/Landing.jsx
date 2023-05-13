a302e546ec5284083ff175910688450faf3498a6
Suzanne <trammelsuzanne@gmail.com>
2023-05-13 18:18:58 -0400

    add landing style

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -15,32 +15,34 @@ const Landing = ({ user, profile }) => {
 
   return (
     <main className={styles.container}>
-      <div className={styles.profile}>
+      <section className={styles.profile}>
         <div className={styles.info}>
           <img src={profile.photo} alt="user" />
           <h2>{profile.name}</h2>
         </div>
         <div className="resume">
-          <h3>Resume:</h3>
+          <h3>Resume</h3>
             <p>add resume</p>
         </div>
         <div className="brand">
-          <h3>Branding Statement:</h3>
+          <h3>Branding Statement</h3>
             <p>add statement</p>
         </div>
       <NavLink to="/auth/change-password">Change Password</NavLink>
-      </div>
-      <section>
-        <div className="resources">
-          <h3>Starred Resources:</h3>
+      </section>
+      <section className={styles.right}>
+        <div className={styles.resources}>
+          {profile.starredResources.map((resource) =>
+          )}
+          <h3>Starred Resources</h3>
           <ul>
             <li>Resource</li>
             <li>Resource</li>
             <li>Resource</li>
           </ul>
         </div>
-        <div className='jobs'>
-          <h3>Applications I'm Working On:</h3>
+        <div className={styles.jobs}>
+          <h3>Applications I'm Working On</h3>
           <ul>
             <li>Job</li>
             <li>Job</li>
