4449bc5354ae72144f5c07921898e3ce044beef1
Suzanne <trammelsuzanne@gmail.com>
2023-05-13 14:09:35 -0400

    stub up profile page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -10,15 +10,35 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   if (!user) return <img src={logo} alt="appliCANt logo" />
 
-  console.log(user)
+  console.log(user.profile)
 
   return (
     <main className={styles.container}>
       <div className="profile-info">
         <img src={user.photo} alt="user" />
-        <h3>{user.name}</h3>
-      </div>
+        <h2>{user.name}</h2>
+        <h3>Resume:</h3>
+          <p>add resume</p>
+        <h3>Branding Statement:</h3>
+          <p>add statement</p>
       <NavLink to="/auth/change-password">Change Password</NavLink>
+      </div>
+      <div className="resources">
+        <h3>Starred Resources:</h3>
+        <ul>
+          <li>Resource</li>
+          <li>Resource</li>
+          <li>Resource</li>
+        </ul>
+      </div>
+      <div className='jobs'>
+        <h3>Applications I'm Working On:</h3>
+        <ul>
+          <li>Job</li>
+          <li>Job</li>
+          <li>Job</li>
+        </ul>
+      </div>
     </main>
   )
 }
