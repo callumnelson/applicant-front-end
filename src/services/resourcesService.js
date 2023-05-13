60306b2e92dcc377605a8bdd4f2d1ec0dd9266da
Nicholas Manning <njmanning212@gmail.com>
2023-05-13 12:32:23 -0500

    create resourceServices for API calls

---
+++ src/services/resourcesService.js
@@ -0,0 +1,117 @@
+// services
+import * as tokenService from './tokenService'
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/resources`
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: {'Authorization': `Bearer ${tokenService.getToken()}`}
+    })
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const create = async (resourceFormData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(resourceFormData),
+    })
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const update = async (resourceFormData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${resourceFormData._id}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(resourceFormData),
+    })
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const deleteResource = async (resourceId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${resourceId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      }
+    })
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const createReview = async (resourceId, reviewFormData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${resourceId}/reviews`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(reviewFormData),
+    })
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const deleteReview = async (resourceId, reviewId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${resourceId}/reviews/${reviewId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+      },
+    })
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const updateReview = async (resourceId, reviewId, reviewFormData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${resourceId}/reviews/${reviewId}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(reviewFormData),
+    })
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export {
+  index,
+  create,
+  update,
+  deleteResource,
+  createReview,
+  deleteReview,
+  updateReview
+}
