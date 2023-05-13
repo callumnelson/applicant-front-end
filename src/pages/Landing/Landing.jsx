89893b1efd55648a899ba15c982a34ac376d881c
Suzanne <trammelsuzanne@gmail.com>
2023-05-13 13:42:21 -0400

    conditionally render logo if not logged in

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,15 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
+//assets
+import logo from '../../assets/branding/logo.svg'
+
 // css
 import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
+  if (!user) return <img src={logo} alt="appliCANt logo" />
+
   return (
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
