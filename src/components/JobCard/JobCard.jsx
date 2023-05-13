23160084b8fa174b39e92cb9ad6c0c03083fc3cb
Callum Nelson <callum.nelson95@gmail.com>
2023-05-13 17:54:09 -0400

    show selected job

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -7,38 +7,51 @@ import styles from './JobCard.module.css'
 const JobCard = ({selectedJob, job, setSelectedJob}) => {
   const selected = selectedJob && selectedJob._id === job._id
   
+  const handleSelect = () => {
+    if(selected) setSelectedJob(null)
+    else setSelectedJob(job)
+  }
+
   return (
-    <div
-      className={
-          `${styles.job} ${selected ? styles.selected : ''}`
-        }>
-      <div>
-        <p
-          className={styles.details}
-          onClick={() => setSelectedJob(job)}
-        >
-          ⬇️
-        </p>
+    <>
+      <div
+        className={
+            `${styles.job} ${selected ? styles.selected : ''}`
+          }>
+        <span>
+          <p
+            className={styles.dropdown}
+            onClick={handleSelect}
+          >
+            {selected ? "⬆️": "⬇️"}
+          </p>
+        </span>
+        <div className={styles.title}>
+          <p>{job.title}</p>
+        </div>
+        <div className={styles.company}>
+          <p>{job.company}</p>
+        </div>
+        <div className={styles.listin}>
+          <p>{job.listing}</p>
+        </div>
+        <div className={styles.status}>
+          <p>{job.status}</p>
+        </div>
+        <div className={styles.priority}>
+          <p>{job.priority}</p>
+        </div>
+        <div className={styles.salary}>
+          <p>{job.salary}</p>
+        </div>
       </div>
-      <div className={styles.title}>
-        <p>{job.title}</p>
-      </div>
-      <div className={styles.company}>
-        <p>{job.company}</p>
-      </div>
-      <div className={styles.listin}>
-        <p>{job.listing}</p>
-      </div>
-      <div className={styles.status}>
-        <p>{job.status}</p>
-      </div>
-      <div className={styles.priority}>
-        <p>{job.priority}</p>
-      </div>
-      <div className={styles.salary}>
-        <p>{job.salary}</p>
-      </div>
-    </div>
+      {
+        selected &&
+        <div className={styles.details}>
+          <h1>Look at my details!!</h1>
+        </div>
+      }
+    </>
   )
 }
  
