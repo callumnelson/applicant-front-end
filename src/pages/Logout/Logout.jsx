6d17acf04f883c9db99f14f1f96b292d5fc72bf3
Suzanne <trammelsuzanne@gmail.com>
2023-05-13 19:10:45 -0400

    touch and stub up logout page

---
+++ src/pages/Logout/Logout.jsx
@@ -0,0 +1,14 @@
+
+
+// css
+import styles from './Logout.module.css'
+
+const Logout = () => {
+  return (
+    <main className={styles.container}>
+      <h1>bye</h1>
+    </main>
+  )
+}
+
+export default Logout
