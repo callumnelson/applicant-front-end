3bf72ac31137928fd5cd28e0c030c4d268175e2f
Suzanne <trammelsuzanne@gmail.com>
2023-05-14 14:29:31 -0400

    make brand input field

--- src/components/BrandForm/BrandForm.jsx
+++ src/components/BrandForm/BrandForm.jsx
@@ -3,8 +3,19 @@ import { useState } from "react";
 import styles from './BrandForm.module.css'
 
 const BrandForm = () => {
+  const [formData, setFormData] = useState({text: ''})
+
   return ( 
-    <h1>I'm a Brandform</h1>
+    <form action="">
+      <label htmlFor="brand"></label>
+      <input 
+        type="text" 
+        name="brand" 
+        value={formData.brand} 
+        placeholder="Add link to branding statment" 
+      />
+      <button type="submit">SUBMIT</button>
+    </form>
   )
 }
 
