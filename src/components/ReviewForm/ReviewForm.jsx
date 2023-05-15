68ea6e9dc0fea93990e59d51a29c4b1a292b31a9
Nicholas Manning <njmanning212@gmail.com>
2023-05-15 09:10:11 -0500

    create review form in component

--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -1,9 +1,51 @@
+import { useState } from "react"
+
 import styles from './ReviewForm.module.css'
 
 const ReviewForm = () => {
+
+  const [formData, setFormData ] = useState({
+    rating: 0,
+    content: '',
+  })
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = (evt) => {}
+
+
   return (  
     <div className={styles.container}>
-      reviewForm 
+      <form onSubmit={handleSubmit}>
+        <div className={styles.content}>
+          <textarea
+            required
+            type="text"
+            name="content"
+            id="text-input"
+            value={formData.content}
+            placeholder="What did you think?"
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.rating}>
+        <select
+          required
+          name="rating"
+          id="category-input"
+          value={formData.rating}
+          onChange={handleChange}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option> 
+        </select>
+        </div>
+      </form>
     </div>
   )
 }
