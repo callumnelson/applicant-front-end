46a3974870c4a17c841216cbcff980e952046441
Callum Nelson <callum.nelson95@gmail.com>
2023-05-15 14:37:50 -0400

    conditionally render header

--- src/components/JobsHeader/JobsHeader.jsx
+++ src/components/JobsHeader/JobsHeader.jsx
@@ -2,19 +2,27 @@
 import styles from './JobsHeader.module.css'
 
 const JobsHeader = ({headers, sort, handleUpdateSort}) => {
+  
+  
   return (
     <header>
       {headers.map(header => (
         <div key={header.col} className={styles[header]}>
-          <h4 
-            id={header.schemaName}
-            onClick={handleUpdateSort}
-          >
-            {header.col} {sort.schemaName === header.schemaName ? 
-              sort.order > 0 ? 
-                '⌃' : '⌄'
-              : ''}
-          </h4>
+          {['title', 'company', 'salary'].includes(header.schemaName) ?
+            <h4 
+              id={header.schemaName}
+              onClick={handleUpdateSort}
+            >
+              {header.col} {sort.schemaName === header.schemaName ? 
+                sort.order > 0 ? 
+                  '⌃' : '⌄'
+                : ''}
+            </h4>
+          : 
+            <h4>
+              {header.col}
+            </h4>
+          }
         </div>
       ))}
     </header>
